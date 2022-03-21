ea12072df9658ac2104fcb59fa9257f8519ae475
Lapko Maksim <maksim.lapko@innowise-group.com>
2022-03-21 08:29:38 +0300

    Initalize repository

---
+++ TweetBook.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace TweetBook.IntegrationTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ TweetBook/Controllers/V1/PostsController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TweetBook.Contracts.V1;
+using TweetBook.Contracts.V1.Requests;
+using TweetBook.Contracts.V1.Responses;
+using TweetBook.Domain;
+using TweetBook.Extensions;
+using TweetBook.Services;
+
+namespace TweetBook.Controllers.V1
+{
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class PostsController : Controller
+    {
+        private IPostService _postService;
+        public PostsController(IPostService postService)
+        {
+            _postService = postService;
+        }
+
+        [HttpGet(ApiRoutes.Posts.GetAll)]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            return Ok(await _postService.GetPostsAsync());
+        }
+
+        [HttpGet(ApiRoutes.Posts.Get)]
+        public async Task<IActionResult> GetPostAsync([FromRoute]Guid postId)
+        {
+            var post = await _postService.GetPostByIdAsync(postId);
+            if (post == null)
+                return NotFound();
+            return Ok(post);
+        }
+
+        [HttpPost(ApiRoutes.Posts.Create)]
+        public async Task<IActionResult> CreateAsync([FromBody]CreatePostRequest postRequest)
+        {
+            var post = new Post
+            {
+                Name = postRequest.Name,
+                UserId = HttpContext.GetUserId()
+            };
+
+            await _postService.CreatePostAsync(post);
+
+            var baseUrl = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host.ToUriComponent()}";
+            var locationUrl = baseUrl + "/" + ApiRoutes.Posts.Get.Replace("{postId}", post.Id.ToString());
+
+            var responsePost = new PostResponse { Id = post.Id };
+            return Created(locationUrl, responsePost);
+        }
+
+        [HttpDelete(ApiRoutes.Posts.Delete)]
+        public async Task<IActionResult> DeleteAsync([FromRoute]Guid postId)
+        {
+            var userOwnsPost = await _postService.UserOwnsPostAsync(postId, HttpContext.GetUserId());
+
+            if (!userOwnsPost)
+                return BadRequest(new AuthFailedResponce
+                {
+                    Errors = new List<string> { "Sorry, you don't own this post!" }
+                });
+
+            var deleted = await _postService.DeletePostAsync(postId);
+            if (!deleted)
+                return NotFound();
+            return NoContent();
+        }
+
+        [HttpPut(ApiRoutes.Posts.Update)]
+        public async Task<IActionResult> UpdateAsync([FromRoute]Guid postId, [FromBody]UpdatePostRequest postRequest)
+        {
+            var userOwnsPost = await _postService.UserOwnsPostAsync(postId, HttpContext.GetUserId());
+
+            if (!userOwnsPost)
+                return BadRequest(new AuthFailedResponce 
+                {
+                    Errors = new List<string> { "Sorry, you don't own this post!" }
+                });
+
+            var post = await _postService.GetPostByIdAsync(postId);
+            post.Name = postRequest.Name;
+
+            var updated = await _postService.UpdatePostAsync(post);
+            if (!updated)
+                return NotFound();
+            return Ok(post);
+        }
+    }
+}
